451104d58df0a3e17cf3787949b0ecd9ca721c18
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-12-21 21:57:29 +0000

    Minor clean up, fcalls = done

--- ast_groundup.py
+++ ast_groundup.py
@@ -13,29 +13,49 @@ def rprint_dict(nested, indent=0):
 
 class AstTreeVisitor(ast.NodeVisitor):
 
-    parse_categories = ["modules", "fdefs", "fcalls"]
+    __parse_categories = ["modules", "fdefs", "fcalls"]
+
+    __bin_ops = {"Add": '+', "Sub": '-', "Mult": '*', "Div": '/', "FloorDiv": "//",
+           "Mod": '%', "Pow": "**", "LShift": "<<", "RShift": ">>",
+           "BitOr": '|', "BitXor": '^', "BitAnd": '&', "MatMult": '@', "Lt": '<', "Gt": '>'}
 
     def __init__(self):
         self.program_dict = OrderedDict()
         self.count_hash = {}
-
-        for pcat in self.parse_categories:
+        for pcat in self.__parse_categories:
             self.program_dict[pcat] = OrderedDict()
             self.count_hash[pcat] = 0
 
-    def __process_call(self, node, call_dict):
-            self.count_hash["fcalls"] += 1
-            call_key = "fcall_{0}".format(self.count_hash["fcalls"])
-            call_dict[call_key] = {}
-            self.__process_args(node.args, call_dict[call_key])
-            call_dict[call_key]["func"] = {}
-            func_details = call_dict[call_key]["func"]
-            if isinstance(node.func, ast.Name):
-                func_details["name"] = node.func.id
-                func_details["lineno"] = node.func.lineno
+    def __process_binop(self, arg):
+        binop_list = []
+        for i in range(3):
+            vargs = list(vars(arg).values())
+            if isinstance(vargs[i], ast.Name):
+                binop_list.append(vargs[i].id)
+            elif isinstance(vargs[i], ast.Num):
+                binop_list.append(vargs[i].n)
             else:
-                func_details["name"] = node.func.attr
-                func_details["lineno"] = node.func.lineno
+                try:
+                    binop_list.append(self.__bin_ops[type(vargs[i]).__name__])
+                except:
+                    binop_list.append(str(vargs[i]))
+
+        return binop_list
+
+    def __process_call(self, node, call_dict, parent=None):
+        self.count_hash["fcalls"] += 1
+        call_key = "fcall_{0}".format(self.count_hash["fcalls"])
+        call_dict[call_key] = {}
+        call_dict[call_key]["parent"] = parent
+        self.__process_args(node.args, call_dict[call_key])
+        call_dict[call_key]["func"] = {}
+        func_details = call_dict[call_key]["func"]
+        if isinstance(node.func, ast.Name):
+            func_details["name"] = node.func.id
+            func_details["lineno"] = node.func.lineno
+        else:
+            func_details["name"] = node.func.attr
+            func_details["lineno"] = node.func.lineno
 
     def __process_args(self, arg_node, call_dict):
         call_dict["args"] = []
@@ -47,12 +67,11 @@ class AstTreeVisitor(ast.NodeVisitor):
             elif isinstance(arg, ast.Call):
                 arg_func = "arg_func_{0}".format(self.count_hash["fcalls"])
                 call_dict[arg_func] = {}
-                self.__process_call(arg, call_dict[arg_func])
+                self.__process_call(arg, call_dict[arg_func], "{0}".format(self.count_hash["fcalls"]))
             elif isinstance(arg, ast.BinOp):
-                pass
-                #l, op, r = npr.process_binop(arg)
-                #bin_op_str = "{0} {1} {2}".format(l, op, r)
-                # self.func_call_dict[node_str]["args"].append(bin_op_str)
+                l, op, r = self.__process_binop(arg)
+                bin_op_str = "{0} {1} {2}".format(l, op, r)
+                call_dict["args"].append(bin_op_str)
             elif isinstance(arg, ast.Str):
                 str_message = "\"" + arg.s + "\""
                 call_dict["args"].append(str_message)
@@ -93,7 +112,8 @@ rprint_dict(fdefs)
 
 print("\n\n**************** Function Call Info ****************\n\n")
 rprint_dict(fcalls)
-print(ast_visitor.count_hash)
+print("No. of distinct function calls in source =", ast_visitor.count_hash["fcalls"])
+
 # outpath = "fdefs.png"
 # source_drawer = SourceDrawer(outpath)
 # source_drawer.fcall_draw(fcalls)
