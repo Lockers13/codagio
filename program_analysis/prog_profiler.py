7ce9c5a674ec295f71d9843290a72ecea6c89777
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-01-19 16:21:29 +0400

    Quick path fix

--- program_analysis/prog_profiler.py
+++ program_analysis/prog_profiler.py
@@ -123,7 +123,7 @@ class Profiler():
             process_lprof_out(output)
             # clean up
             os.remove(pro_file)
-            os.remove("./{0}.lprof".format(pro_file.split("/")[-1]))
+            os.remove("{0}.lprof".format(os.path.basename(pro_file)))
     
         # check that line profiler is installed for kernprof
         try:
