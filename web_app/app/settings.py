adfe8dac3f47357c167fe0a48860fdab00266edf
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-02-06 14:33:59 +0400

    changed name of main app from django_backend to app

--- web_app/app/settings.py
+++ web_app/app/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_backend.urls'
+ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_backend.wsgi.application'
+WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # Database
--- web_app/app/wsgi.py
+++ web_app/app/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'django_backend.settings')
+                      'app.settings')
 
 application = get_wsgi_application()
