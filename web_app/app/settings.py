3fae3021150d7b7f0de318a1b9e5c95f7f17f96c
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-02-07 23:38:06 +0400

    Added commented out sqlite3 DB in settings.py

--- web_app/app/settings.py
+++ web_app/app/settings.py
@@ -90,6 +90,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
