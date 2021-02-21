e0c035138c127333fd078fa8203239e7df865cdb
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-02-21 21:12:04 +0400

    Quick frontend fixes

--- web_app/accounts/urls.py
+++ web_app/accounts/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
