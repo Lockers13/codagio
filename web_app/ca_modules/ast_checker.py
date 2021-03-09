cbe130c49fb3545a30ce33f2d36a5847fffa9357
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-03-09 19:37:57 +0000

    quick update to ast_checker

--- web_app/ca_modules/ast_checker.py
+++ web_app/ca_modules/ast_checker.py
@@ -3,13 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def validate(ast_analysis, filename):
-    if len(ast_analysis["fdefs"].keys()) != 1:
-        os.remove(filename)
-        return Response("POST NOT OK: more or less than 1 function!", status=status.HTTP_400_BAD_REQUEST)
+    os.remove(filename)
+    num_fdefs = len(ast_analysis["fdefs"].keys())
+    constraints_vlen = len(ast_analysis["constraint_violation"])
+    if num_fdefs != 1:
+        return Response("POST NOT OK: Too few or too many function(s) => {0}".format(num_fdefs), status=status.HTTP_400_BAD_REQUEST)
     # see if ast visitor found any constraint violations
-    if len(ast_analysis["constraint_violation"]) > 0:
-        os.remove(filename)
+    if constraints_vlen > 0:
         return Response("POST NOT OK: contraint violation(s) : {0}" .format(ast_analysis["constraint_violation"]), status=status.HTTP_400_BAD_REQUEST)
     ### remove old basic file and create more sophisticated one for verification and profiling
-    os.remove(filename)
     return True
