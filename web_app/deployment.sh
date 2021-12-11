5209727c80ed847e27068a8653684502c8d96e1e
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-12-11 19:36:09 +0000

    Updated deployment script

--- web_app/deployment.sh
+++ web_app/deployment.sh
@@ -2,17 +2,20 @@ git clone https://github.com/Lockers13/codagio
 cd codagio
 git checkout portf_deployment
 python3 -m venv env; . env/bin/activate
-pip3 install wheel
-pip3 install -r requirements.txt
-echo $PUB_IP #! copy-paste
-ALLOWED_HOSTS = [$PUB_IP]
-DB_USER => postgres
-#! DB => sudo -u postgres psql postgres
-#! python manage.py makemigrations
-#! python manage.py migrate
-for i in $(find static/js -name "*.js"); do sed -i 's/localhost/$PUB_IP/g' "$i"; done
-#! make sample_tutor superuser
+pip3 install wheel; pip3 install -r requirements.txt
+HOST_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+cd web_app
+sed -i 's/ALLOWED_HOSTS = []/ALLOWED_HOSTS = [$HOST_IP]/g' app/settings.py
+sed -i "s/'USER': 'lorcan'/'USER': 'postgres'/g" app/settings.py
+# echo $PUB_IP #! copy-paste
+#
+# ALLOWED_HOSTS = [$PUB_IP]
+# DB_USER => postgres
+# DB => sudo -u postgres psql postgres
+# python manage.py makemigrations
+# python manage.py migrate
+for i in $(find static/js -name "*.js"); do sed -i 's/localhost/$HOST_IP/g' "$i"; done
+# make sample_tutor superuser
 
-#! TIMEOUT -m ON GITHUB
-#! MAKE REGISTRATION/LOGIN EXPERIENCE BETTER
-#! LOGIN CREDS: lorcan, cgcexample; sample_tutor, on*************; course: MIT_CS201, sample_course123
+# MAKE REGISTRATION/LOGIN EXPERIENCE BETTER
+# LOGIN CREDS: lorcan, cgcexample; sample_tutor, on*************; course: MIT_CS201, sample_course123
