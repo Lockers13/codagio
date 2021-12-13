a9a838ecb98a72b9e16b4c25981fe741c35c9801
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-12-13 14:58:30 +0000

    Quick display/stylistic changes

--- web_app/accounts/forms.py
+++ web_app/accounts/forms.py
@@ -8,7 +8,7 @@ ROLES=[('student','Student'),
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
-	role = forms.ChoiceField(choices=ROLES, widget=forms.RadioSelect, required=True)
+#	role = forms.ChoiceField(choices=ROLES, widget=forms.RadioSelect, required=True)
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
@@ -16,7 +16,7 @@ class NewUserForm(UserCreationForm):
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
-		user.role = self.cleaned_data['role']
+		user.role = "student" # self.cleaned_data['role']
 		if commit:
 			user.save()
 		return user
